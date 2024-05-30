77399e064bd640a1e9c1998dc36e30f768b6fbab
Michael Vasallo <mjrvasallo@gmail.com>
2024-05-30 15:49:39 -0500

    Improve readability

--- app/services/availability_service.rb
+++ app/services/availability_service.rb
@@ -1,15 +1,15 @@
 require_relative '../models/appointment'
 
 class AvailabilityService
-  CLINIC_OPEN = 9
-  CLINIC_CLOSE = 17
-  SLOT_GAP = 30 * 60
-  BOOKING_CUTOFF_TIME = 120 * 60
+  CLINIC_OPEN_HOUR = 9
+  CLINIC_CLOSE_HOUR = 17
+  SLOT_DURATION_SECONDS = 30 * 60
+  BOOKING_CUTOFF_SECONDS = 120 * 60
 
   def initialize(schedule, type, date)
     @appointment_duration = Appointment::DURATIONS[type]
-    @current_slot = [Time.new(date.year, date.month, date.day, CLINIC_OPEN, 0, 0), next_possible_slot].max
-    @end_time = Time.new(date.year, date.month, date.day, CLINIC_CLOSE, 0, 0)
+    @current_slot = [clinic_opening_time(date), next_bookable_slot].max
+    @end_time = clinic_closing_time(date)
     @booked_appointments = schedule.appointments_on(date)
     @booked_index = find_starting_index
   end
@@ -17,13 +17,12 @@ class AvailabilityService
   def call
     available_slots = []
 
-    while @current_slot + @appointment_duration <= @end_time
+    while slot_within_clinic_hours?
       if overlaps_with_booked_appointment?
-        @current_slot = @booked_appointments[@booked_index].end_time
-        @booked_index += 1
+        advance_to_next_slot_after_booked_appointment
       else
         available_slots << @current_slot
-        @current_slot += SLOT_GAP
+        advance_to_next_slot
       end
     end
 
@@ -31,6 +30,18 @@ class AvailabilityService
   end
 
   private
+
+  def clinic_opening_time(date)
+    Time.new(date.year, date.month, date.day, CLINIC_OPEN_HOUR, 0, 0)
+  end
+
+  def clinic_closing_time(date)
+    Time.new(date.year, date.month, date.day, CLINIC_CLOSE_HOUR, 0, 0)
+  end
+
+  def next_bookable_slot
+    Time.at((Time.now + BOOKING_CUTOFF_SECONDS + SLOT_DURATION_SECONDS).to_i / SLOT_DURATION_SECONDS * SLOT_DURATION_SECONDS)
+  end
 
   def find_starting_index
     @booked_appointments.bsearch_index { |appt| appt.end_time > @current_slot } || @booked_appointments.size
@@ -41,7 +52,16 @@ class AvailabilityService
       @current_slot + @appointment_duration > @booked_appointments[@booked_index].start_time
   end
 
-  def next_possible_slot
-    Time.at((Time.now + BOOKING_CUTOFF_TIME + SLOT_GAP).to_i / SLOT_GAP * SLOT_GAP)
+  def slot_within_clinic_hours?
+    @current_slot + @appointment_duration <= @end_time
+  end
+
+  def advance_to_next_slot_after_booked_appointment
+    @current_slot = @booked_appointments[@booked_index].end_time
+    @booked_index += 1
+  end
+
+  def advance_to_next_slot
+    @current_slot += SLOT_DURATION_SECONDS
   end
 end
